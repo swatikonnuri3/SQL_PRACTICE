e90c4eaf05f043170aa6d17a196cf147f057cb1c
swati konnuri <swatikonnuri@gmail.com>
2026-02-23 13:47:07 +0530

    created table structure using query

---
+++ src/basic_sql/create_database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE classrooms;
+USE classrooms;
+CREATE TABLE students(
+    id INT PRIMARY KEY ,
+    name VARCHAR(50),
+    age INT NOT NULL
+);
+INSERT INTO students VALUES(1,"swati",21);  
+INSERT INTO students VALUES(2,"sneha",25);  
